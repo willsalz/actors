c864caa95289ff1a6577988b878e1fae0a8732c6
Will Salisbury <wjs@uber.com>
2017-01-19 15:38:00 -0800

    Logging and Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-#![feature(unboxed_closures, fn_traits)]
+#![feature(unboxed_closures, fn_traits, rustc_private)]
+
+#[macro_use]
+extern crate log;
 
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
@@ -17,12 +20,15 @@ trait Actor {
 
 struct ActorHarness<T: Actor> {
     inbox: Receiver<Message>,
-    actor: T
+    actor: T,
 }
 
 impl<T: Actor> ActorHarness<T> {
     fn new(inbox: Receiver<Message>, actor: T) -> ActorHarness<T> {
-        ActorHarness{inbox: inbox, actor: actor}
+        ActorHarness {
+            inbox: inbox,
+            actor: actor,
+        }
     }
 }
 
@@ -36,21 +42,21 @@ impl<T: Actor> FnOnce<()> for ActorHarness<T> {
                 Ok(m) => self.actor.handle(m),
                 // Exit on error
                 Err(e) => {
-                    println!("[Actor] Error: {:?}", e);
+                    error!("[Actor] Error: {:?}", e);
                     break;
-                },
+                }
             }
         }
     }
 }
 
 struct ActorRef {
-    outbox: Sender<Message>
+    outbox: Sender<Message>,
 }
 
 impl ActorRef {
     fn new(outbox: Sender<Message>) -> ActorRef {
-        ActorRef{outbox: outbox}
+        ActorRef { outbox: outbox }
     }
 }
 
@@ -67,13 +73,13 @@ impl Actor for ActorImpl {
     fn handle(&self, message: Message) -> () {
         match message {
             // If we're given a 'special' -1 value, exit.
-            Message{payload, sender: _} if payload == -1 => {
-                println!("[Actor] Exiting!");
+            Message { payload, sender: _ } if payload == -1 => {
+                info!("[Actor] Exiting!");
                 panic!("Get me out of here!");
-            },
+            }
             // Otherwise, print and respond to the message!
-            Message{payload, sender} => {
-                println!("[Actor] Got: {:?}", payload);
+            Message { payload, sender } => {
+                info!("[Actor] Got: {:?}", payload);
                 sender.send(payload + 1).unwrap();
             }
         }
@@ -97,19 +103,34 @@ fn main() {
 
     // Communication channel with actor
     let (sender, receiver): (Sender<Payload>, Receiver<Payload>) = mpsc::channel();
-    r.outbox.send(Message{payload: 0, sender: sender.clone()}).unwrap();
+    r.outbox
+        .send(Message {
+            payload: 0,
+            sender: sender.clone(),
+        })
+        .unwrap();
     let num = receiver.recv().unwrap();
-    println!("[Main] Got: {:?}", num);
+    info!("[Main] Got: {:?}", num);
 
     // Send another number!
-    r.outbox.send(Message{payload: num + 1, sender: sender.clone()}).unwrap();
+    r.outbox
+        .send(Message {
+            payload: num + 1,
+            sender: sender.clone(),
+        })
+        .unwrap();
     let num = receiver.recv().unwrap();
-    println!("[Main] Got: {:?}", num);
+    info!("[Main] Got: {:?}", num);
 
     // Send exit code
-    r.outbox.send(Message{payload: -1, sender: sender.clone()}).unwrap();
+    r.outbox
+        .send(Message {
+            payload: -1,
+            sender: sender.clone(),
+        })
+        .unwrap();
 
     // Wait for thread to exit
     let res = handle.join();
-    println!("[Main] Res: {:?}", res);
+    info!("[Main] Res: {:?}", res);
 }
